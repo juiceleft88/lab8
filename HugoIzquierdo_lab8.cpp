5d047f323a036b7245ffa43493b66de017010b6c
juiceleft88 <juiceleft88@gmail.com>
2021-02-04 07:37:13 -0800

    Beginning framework

---
+++ HugoIzquierdo_lab8.cpp
@@ -0,0 +1,19 @@
+//Hugo Izquierdo
+//February 3rd, 2021
+//This program will help the user create and check validity of a strong password 
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
