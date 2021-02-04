84ca4a58e1629bb4f129626847c20abae69e76e6
juiceleft88 <juiceleft88@gmail.com>
2021-02-04 09:36:18 -0800

    isdigit function testing

--- HugoIzquierdo_lab8.cpp
+++ HugoIzquierdo_lab8.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 void sizeCheck(char[], int);
+bool digitCheck(char[], int)
 
 int main()
 {
@@ -20,6 +21,13 @@ int main()
             "\n>One lowercase letter \n>One number \n>One special character (punctuation)" << endl;
     cin >> password;
     sizeCheck(password, SIZE);
+
+    while(digitCheck(password, SIZE) == false)
+    {
+        cout << "You are missing a numerical value. please re-enter password: " <<endl;
+        cin >> password;
+    }
+    
 
     
 
@@ -37,6 +45,13 @@ void sizeCheck(char custPass[], int size)
         cout << "Your password is not following one of the above requirements. Please enter again: " << endl;
         cin >> custPass;
     }
+}
+
+bool digitCheck(char custPass[], int size)
+{
+    for (int count = 0; count < size; count++)
+        if (!isdigit(custPass[count]))
+        return false;
 }
 
 /*
